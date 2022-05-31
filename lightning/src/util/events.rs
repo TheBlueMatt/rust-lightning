c7d078c0220506540c4b7d5b38ac073ccb596387
Elias Rohrer <ero@tnull.de>
2022-05-30 17:23:11 -0700

    Fix docs

--- lightning/src/util/events.rs
+++ lightning/src/util/events.rs
@@ -502,7 +502,7 @@ pub enum Event {
 		/// Furthermore, note that if [`ChannelTypeFeatures::supports_zero_conf`] returns true on this type,
 		/// the resulting [`ChannelManager`] will not be readable by versions of LDK prior to
 		/// 0.0.107. Channels setting this type also need to get manually accepted via
-		/// [`ChannelManager::accept_inbound_channel_from_trusted_peer_0conf`], or will be rejected
+		/// [`crate::ln::channelmanager::ChannelManager::accept_inbound_channel_from_trusted_peer_0conf`], or will be rejected
 		/// otherwise.
 		///
 		/// [`ChannelManager`]: crate::ln::channelmanager::ChannelManager
